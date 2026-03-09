e5f979acec456e76e805cfa7d1d2460c90936fe9
TrHuyD <trhuy210702@gmail.com>
2026-03-09 17:24:30 +0700

    add sonner (toaster)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import {Inter} from 'next/font/google'
 import Navbar from '@/components/ui/Navbar'
+import { Toaster } from 'sonner'
 export const metadata = {
   title: 'Redit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
@@ -18,11 +19,11 @@ export default function RootLayout({
     <html lang='en' className= {cn('text-slate-900 bg-white antialiased light', inter.className)}>
       <body className ={cn('min-h-screen pt-12 bg-slate-50 antialiased')}>
        <Navbar/>
-        <div className={cn('container max-w-7x1 mx-auto h-full pt-12')}>
+        <div className={cn('container max-w-7xl mx-auto h-full pt-12')}>
           
           {children}
         </div>
-
+      <Toaster/>
       </body>
     </html>
   )
