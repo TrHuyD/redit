470474a110853222679b534981986d4911a1fd29
TrHuyD <trhuy210702@gmail.com>
2026-03-10 20:08:34 +0700

    Add main logo

---
+++ src/components/ui/Icons.tsx
@@ -0,0 +1,16 @@
+import { Apple } from "lucide-react"
+import { cn } from "@/lib/utils"
+
+type TSize = "sm" | "md" | "lg"
+type LogoProps = {
+  size?: TSize
+  className?: string
+}
+const logoSizeMap: Record<TSize, string> = {
+  sm: "h-5 w-5",
+  md: "h-8 w-8",
+  lg: "h-12 w-12",
+}
+export default function Logo({ size = "md", className }: LogoProps) {
+  return <Apple className={cn(logoSizeMap[size], "fill-red-500", className)} />
+}
