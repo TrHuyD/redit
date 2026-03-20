92eb544701828e1479d7b6563bf9152a574c1806
TrHuyD <trhuy210702@gmail.com>
2026-03-20 20:49:01 +0700

    remove minor log

--- src/server/services/subreddit/vote.ts
+++ src/server/services/subreddit/vote.ts
@@ -3,6 +3,7 @@ import { Result } from "@/lib/Result"
 import { PostVoteRequestPayload } from "@/lib/validators/post"
 
 export async function VotePost({type,postId,userId}: PostVoteRequestPayload) : Promise<Result<null>>{
+    console.log(type)
     await db.postVote.upsert({ 
         where: { postId_userId: { postId, userId, }, }, 
         update: { type: type }, 
