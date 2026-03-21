8283ff02e81ca9d63d3e56420a7a98ca3f6a893d
TrHuyD <trhuy210702@gmail.com>
2026-03-21 16:27:43 +0700

    fix typo

--- src/server/services/subscription/GetSubscription.tsx
+++ src/server/services/subscription/GetSubscription.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db"
+import { ID } from "@/types/ID"
 
-export async function getSubscription(slug: string, userId: string) {
+export async function getSubscription(slug: string, userId: ID) {
     return db.subscription.findFirst({
       where: {
         subreddit: { name: slug },
@@ -9,7 +10,7 @@ export async function getSubscription(slug: string, userId: string) {
     })
   }
   
-  export async function isMember(slug: string, userId: string): Promise<boolean> {
+  export async function isMember(slug: string, userId: ID): Promise<boolean> {
     const subscription = await getSubscription(slug, userId)
     return !!subscription
   }
