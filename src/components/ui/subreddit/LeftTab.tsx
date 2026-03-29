e2f2f8b36abf89e14d70982e879bb1d06e89b1f9
TrHuyD <trhuy210702@gmail.com>
2026-03-30 00:02:28 +0700

    add: icons in left tab

--- src/components/ui/subreddit/LeftTab.tsx
+++ src/components/ui/subreddit/LeftTab.tsx
@@ -4,6 +4,8 @@ import CreateSubReddit from '@/app/r/create/components/createSubReddit'
 import Link from 'next/link'
 import { useModal } from '../providers/modal-provider'
 import { RecentSubreddits } from '../user/RecentSubreddits'
+import { Home, Plus, Sparkle } from 'lucide-react'
+
 
 interface Props {
 }
@@ -11,20 +13,27 @@ interface Props {
 export function LeftTab() {
   const { openModal, closeModal } = useModal()
   return (
-    <div className="hidden md:flex flex-col gap-6 sticky top-20 text-sm">
+    <div className="hidden md:flex flex-col gap-6 sticky top-20 text-sm px-5">
 
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-1 ">
         <p className="text-xs text-zinc-500 px-2">FEEDS</p>
 
         <Link href="/" className="px-3 py-2 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-800">
+          <div className='flex gap-1'>
+          <Home className="w-5 h-5" />
           Home
+          </div>
         </Link>
 
         <Link href="/r/all" className="px-3 py-2 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-800">
-          Everyone
+           <div className='flex gap-1'>
+         <Sparkle className="w-5 h-5" /> Everyone
+         </div>
         </Link>
         <button onClick={() => openModal(<CreateSubReddit />)}className="px-3 py-2 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-800 text-left">
-          Create new community
+         <div className='flex gap-1'>
+           <Plus className="w-5 h-5" /> Create a new community
+          </div>
         </button>
       </div>
 
