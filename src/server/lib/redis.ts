48bb3d564329f346c609bfd9cf621924087eb904
TrHuyD <trhuy210702@gmail.com>
2026-04-09 04:31:02 +0700

    adjust: improve redis's connection logic

--- src/server/lib/redis.ts
+++ src/server/lib/redis.ts
@@ -1,29 +1,44 @@
-import Redis from "ioredis"
+import Redis from "ioredis";
 import "@/lib/bigint";
 declare global {
-  var _redis: Redis | undefined
+  var _redis: Redis | undefined;
   interface BigInt {
     toJSON(): string;
   }
 }
-function createRedisInstance(){
-  if(process.env.REDIS_TYPE=="UPSTASH")
-    return new Redis(process.env.REDIS_UPSTASH!)
-  return  new Redis({
+function createRedisInstance() {
+  if (process.env.REDIS_TYPE === "UPSTASH") {
+    if (!process.env.REDIS_UPSTASH) {
+      throw new Error("Missing REDIS_UPSTASH");
+    }
+
+    return new Redis(process.env.REDIS_UPSTASH, {
+      maxRetriesPerRequest: 2,
+      enableReadyCheck: false,
+      lazyConnect: true,
+    });
+  }
+
+  if (process.env.REDIS_URL?.startsWith("redis://")) {
+    return new Redis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: 2,
+      enableReadyCheck: false,
+      lazyConnect: true,
+    });
+  }
+
+  return new Redis({
     host: process.env.REDIS_URL,
     port: 15551,
     username: "default",
     password: process.env.REDIS_SECRET,
     maxRetriesPerRequest: 2,
-    enableReadyCheck: true,
-  })
-
-
-
+    enableReadyCheck: false,
+    lazyConnect: true,
+  });
 }
+export const redis = global._redis ?? createRedisInstance();
 
-export const redis =
-  global._redis ??createRedisInstance()
 if (process.env.NODE_ENV !== "production") {
-  global._redis = redis
+  global._redis = redis;
 }
